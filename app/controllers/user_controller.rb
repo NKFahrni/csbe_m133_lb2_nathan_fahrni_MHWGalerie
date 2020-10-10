5fe971ac859cfdbf3f49cf240e76fed769a1f538
4row <nathan.fahrni@csbe.ch>
2020-10-10 18:42:48 +0200

    Profil mit Update user und delete user angefangen

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,6 @@
 class UserController < ApplicationController #SRC: https://3rd-edition.railstutorial.org/book/sign_up
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only:[:edit, :update]
   def new
     @user = User.new
   end
@@ -12,9 +14,44 @@ class UserController < ApplicationController #SRC: https://3rd-edition.railstuto
     end
   end
 
+  def edit  #SRC: https://3rd-edition.railstutorial.org/book/updating_and_deleting_users
+    @user = User.find_by_id(params[:id])
+  end
+
+  # Confirms correct user SRC: https://3rd-edition.railstutorial.org/book/updating_and_deleting_users
+  def correct_user
+    @user = User.find_by_id([:id])
+    redirect_to(root_path) unless @user == currentuser?(@user)
+  end
+
+  def update #handles successful edit/update SRC: https://3rd-edition.railstutorial.org/book/updating_and_deleting_users
+    @user = User.find_by_id(params[:id])
+    if @user.update_attributes(user_params)
+
+      redirect_to root_path
+
+    else
+      render 'profil'
+    end
+  end
+
+  def destroy
+    User.find_by_id(params[:id]).destroy
+    logout
+    redirect_to root_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :username, :password, :password_confirmation)
+  end
+
+  #checks if user is logged in SRC: https://3rd-edition.railstutorial.org/book/updating_and_deleting_users
+  def logged_in_user
+    unless  loggedin?
+      store_location
+      redirect_to 'login/index_url'
+    end
   end
 end
 
--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -1,19 +1,39 @@
 module SessionHelper
-  # Logs in the current/given User
+
+  # Logs in the current/given User SRC: https://3rd-edition.railstutorial.org/book/log_in_log_out
   def login(user)
     session[:user_id] = user.id
   end
-  # Returns the current Loggedf-in user if any are logged in
+
+  #returns true if the user is the current user SRC: https://3rd-edition.railstutorial.org/book/updating_and_deleting_users
+  def currentuser?(user)
+    user == currentuser
+  end
+
+  # Returns the current Logged-in user if any are logged in SRC: https://3rd-edition.railstutorial.org/book/log_in_log_out
   def currentuser
-    @currentuser ||=user.nifd_by(id: session[:user_id])
+    @currentuser = User.find_by(id: session[:user_id])
   end
-  #Returns true if the user is logged in, false when otherwise
+
+  #Returns true if the user is logged in, false when otherwise SRC: https://3rd-edition.railstutorial.org/book/log_in_log_out
   def loggedin?
-    !currentuser-nil?
+    !currentuser = nil?
   end
-  #logs the xurrent user out
+
+  #logs the current user out SRC: https://3rd-edition.railstutorial.org/book/log_in_log_out
   def logout
     session.delete(:user_id)
     @currentuser  = nil
+  end
+
+  #redirects to stored location (or to the default)
+  def redirect_back_to(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  #stores the URL trying to be aaccessed
+  def store_location
+    session[:forwarding_url] = request_url if request.get?
   end
 end
