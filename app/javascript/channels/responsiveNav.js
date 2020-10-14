fd5782634b17d71a152f81dce2bf1850f2e1b585
4row <nathan.fahrni@csbe.ch>
2020-10-14 14:45:54 +0200

    Tests geprüft.

--- app/javascript/channels/responsiveNav.js
+++ app/javascript/channels/responsiveNav.js
@@ -1,7 +1,16 @@
-const hamburger = document.getElementById("hamburger");
-const navUl =  document.getElementById("nav-ul");
+//const hamburger = document.getElementById("hamburger");
+//const navUl =  document.getElementById("nav-ul");
 
-hamburger.addEventListener('click', () => {
-    navUl.classList.toggle('show');
-})
+//hamburger.addEventListener('click', () => {
+//    navUl.classList.toggle('show');
+//})
+
+function myFunction(){
+    var x = document.getElementById("mynav_bar")
+    if (x.className === "nav_bar"){
+        x.className += " responsive";
+    }else {
+        x.className = "nav_bar";
+    }
+}
 
